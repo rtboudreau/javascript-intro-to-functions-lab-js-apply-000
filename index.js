ffee774e13c39e940472cea5c9c3544138f52470
Reid Boudreau <rtboudreau@gmail.com>
2018-12-24 15:57:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,14 +14,3 @@ function logShout(string) {
   console.log(string.toUpperCase());
 }
 
-function sayHiToGrandma(string){
-    if (string.toLowerCase() === string){
-      return 'I can\'t hear you!';
-    }
-    if (string.toUpperCase() === string){
-      return 'YES INDEED!'
-    }
-    if (string === "I love you, Grandma."){
-      return "I love you, too."
-    }
-}
